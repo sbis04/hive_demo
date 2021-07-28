24a8a98c24e33c083268cc9772661ea9429c871d
Souvik Biswas <sbis1999@gmail.com>
2021-07-28 07:58:04 +0530

    Add person update form

---
+++ lib/utils/update_person_form.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
+import 'package:hive_demo/models/person.dart';
+
+final peopleFormKey = GlobalKey<FormState>();
+
+class UpdatePersonForm extends StatefulWidget {
+  final int index;
+  final Person person;
+
+  const UpdatePersonForm({
+    required this.index,
+    required this.person,
+  });
+
+  @override
+  _UpdatePersonFormState createState() => _UpdatePersonFormState();
+}
+
+class _UpdatePersonFormState extends State<UpdatePersonForm> {
+  late final _nameController;
+  late final _countryController;
+  late final Box box;
+
+  String? _fieldValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Field can\'t be empty';
+    }
+    return null;
+  }
+
+  // Update info of people box
+  _updateInfo() {
+    Person newPerson = Person(
+      name: _nameController.text,
+      country: _countryController.text,
+    );
+
+    box.putAt(widget.index, newPerson);
+
+    print('Info updated in box!');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Get reference to an already opened box
+    box = Hive.box('peopleBox');
+    _nameController = TextEditingController(text: widget.person.name);
+    _countryController = TextEditingController(text: widget.person.country);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: peopleFormKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Name'),
+          TextFormField(
+            controller: _nameController,
+            validator: _fieldValidator,
+          ),
+          SizedBox(height: 24.0),
+          Text('Home Country'),
+          TextFormField(
+            controller: _countryController,
+            validator: _fieldValidator,
+          ),
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 24.0),
+            child: Container(
+              width: double.maxFinite,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  if (peopleFormKey.currentState!.validate()) {
+                    _updateInfo();
+                    Navigator.of(context).pop();
+                  }
+                },
+                child: Text('Update'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
