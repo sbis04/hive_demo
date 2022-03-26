e42257a74d5745b6b41b77f6f5c3f117580f12b4
Souvik Biswas <sbis1999@gmail.com>
2022-03-26 22:09:26 +0530

    Hive encrypted box example

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,33 @@
-import 'package:flutter/material.dart';
-import 'package:hive_demo/models/person.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+import 'dart:convert';
+import 'dart:developer';
 
-import 'screens/info_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 main() async {
   // Initialize hive
   await Hive.initFlutter();
-  // Registering the adapter
-  Hive.registerAdapter(PersonAdapter());
-  // Opening the box
-  await Hive.openBox('peopleBox');
+
+  const secureStorage = FlutterSecureStorage();
+  final encryprionKey = await secureStorage.read(key: 'key');
+
+  if (encryprionKey == null) {
+    final key = Hive.generateSecureKey();
+    await secureStorage.write(
+      key: 'key',
+      value: base64UrlEncode(key),
+    );
+  }
+
+  final key = await secureStorage.read(key: 'key');
+  final encryptionKey = base64Url.decode(key!);
+  print('Encryption key: $encryptionKey');
+
+  await Hive.openBox<String>(
+    'securedBox',
+    encryptionCipher: HiveAesCipher(encryptionKey),
+  );
 
   runApp(MyApp());
 }
@@ -36,7 +53,66 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.purple,
       ),
       debugShowCheckedModeBanner: false,
-      home: InfoScreen(),
+      home: EncryptedBoxTest(),
+    );
+  }
+}
+
+class EncryptedBoxTest extends StatefulWidget {
+  const EncryptedBoxTest({Key? key}) : super(key: key);
+
+  @override
+  State<EncryptedBoxTest> createState() => _EncryptedBoxTestState();
+}
+
+class _EncryptedBoxTestState extends State<EncryptedBoxTest> {
+  late final Box<String> encryptedBox;
+  String? data = '';
+
+  _getData() {
+    setState(() {
+      data = encryptedBox.get('secret');
+    });
+    log('Fetched data');
+  }
+
+  _putData() async {
+    await encryptedBox.put('secret', 'Test secret key');
+    log('Stored data');
+  }
+
+  @override
+  void initState() {
+    encryptedBox = Hive.box<String>('securedBox');
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('$data'),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  onPressed: _getData,
+                  child: Text('Get'),
+                ),
+                ElevatedButton(
+                  onPressed: _putData,
+                  child: Text('Store'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_secure_storage_macos
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
